631eb8e47e1e6ee99645f891ed8b9e2c82fedbf8
Mariabarbosa12 <160647209+Mariabarbosa12@users.noreply.github.com>
2025-06-10 11:41:48 -0300

    aula 10-06

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,13 +1,10 @@
-import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
+import { Geist } from "next/font/google";
+
+import "../styles/globals.css";
+import "../styles/reset.css";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
   subsets: ["latin"],
 });
 
@@ -18,8 +15,8 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
+    <html lang="pt-br">
+      <body className={`${geistSans.variable}`}>
         {children}
       </body>
     </html>
--- src/app/page.js
+++ src/app/page.js
@@ -3,8 +3,10 @@ import styles from "./page.module.css";
 
 export default function Home() {
   return (
-    <div className={styles.page}>
-      
+    <div className=" Container">
+      <h1>MARIA JULIA</h1>
+      <h2>MARIA JULIA</h2>
+      <h2>MARIA JULIA</h2>
     </div>
   );
 }
