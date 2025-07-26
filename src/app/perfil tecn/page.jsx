839c150558acafe316fb7bc8d57769cbe58e8b50
Maria Julia <maria.barbosa315@etec.sp.gov.br>
2025-07-26 15:34:36 -0300

    teste 26-07

---
+++ src/app/perfil tecn/page.jsx
@@ -0,0 +1,10 @@
+
+import styles from "./page.module.css";
+
+export default function perfil() {
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.titulo}>PERFIL</h1>
+        </div>
+  );
+}
