307cb0117258fffc755c932ae69500970d0dbc96
Maria Julia <mariajuliasantosbarbosa40@gmail.com>
2025-08-08 14:03:47 -0300

    089-08

--- src/app/entradaa/page.jsx
+++ src/app/entradaa/page.jsx
@@ -1,10 +1,28 @@
-
 import styles from "./page.module.css";
 
-export default function entrada() {
+export default function Entrada() {
   return (
-    <div className={styles.container}>
-      <h1 className={styles.titulo}>ENTRADA</h1>
-         </div>
+    <div className={styles.body}>
+      <div className={styles.container}>
+        <div className={styles.iconButton}>
+          <img className={styles.imgl} src="/logoredonda.png" alt="Logo BLUVA" />
+          <h1 className={styles.titulo}>BLUVA</h1>
+        </div>
+
+        <select id="tipoUsuario" className={styles.select}>
+          <option value="">Selecione</option>
+          <option value="usuario">Usuário</option>
+          <option value="tecnico">Técnico</option>
+        </select>
+
+        <button
+          className={styles.button}
+          onClick={() => (window.location.href = "/login")}
+        >
+          Prosseguir
+        </button>
+      </div>
+    </div>
   );
 }
+
