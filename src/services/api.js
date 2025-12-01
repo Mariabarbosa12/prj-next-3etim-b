7c4272183025daac8613227ee17dac225762d99b
Maria Julia dos Santos Barbosa <160647209+Mariabarbosa12@users.noreply.github.com>
2025-12-01 13:30:06 -0300

    Revert "erro cadastro"
    This reverts commit ea8778135941cba92fe8034676e073dc87a59209.

--- src/services/api.js
+++ src/services/api.js
@@ -1,12 +1,9 @@
 import axios from "axios";
 
-const apiUrl = process.env.NEXT_PUBLIC_API_URL;
-const apiPorta = process.env.NEXT_PUBLIC_API_PORTA; // 2. Importar a porta
-
-const baseURL = `${apiUrl}:${apiPorta}`; 
-const linkApi = baseURL || 'http://localhost:3333'; 
+const linkApi = process.env.NEXT_PULIC_API_URL || 'http://localhost:3333'; 
 
 const api = axios.create({
+    baseURL: linkApi,   
 });
 
 export default api;
